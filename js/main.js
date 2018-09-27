2c0e628ae6c99368fe4b0c2775d62f3ac6a2fa70
Micah Yunana Bala <micahkulien@gmail.com>
2018-09-27 08:53:25 +0100

    Update click functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-// Select card picture
+// Select card pictures
 let catPic = document.querySelectorAll('.card');
+// Track the number of clicks on each cat
 let clicksOnCat = 0;
 
+// function to update the number of clicks on the page beside each cat picture
 function numberOfClicks(targetCat, numClicks) {
   // Select the class holding the cat image
   let myCat = document.querySelector(`.${targetCat}`);
@@ -15,10 +17,17 @@ function numberOfClicks(targetCat, numClicks) {
   elem.textContent = numClicks;
 }
 
+// function to Check which cat picture was clicked
 function catClicks(e) {
+  // Get the class name of the clicked element holding the picture
   let clickedCat = e.target.className;
+
+  // Checks if the clicked element has class
   if (clickedCat === 'first_cat') {
     clicksOnCat++;
+    /* This is where i think my problem starts - Increments the number of clicks and pass
+    * it to the called method along with a reference to the element clicked.
+     */
     numberOfClicks(clickedCat, clicksOnCat);
   }
 
@@ -28,6 +37,7 @@ function catClicks(e) {
   }
 }
 
+// Loop through the collection of cat pictures and listen for click event
 catPic.forEach(cat => {
   cat.addEventListener('click', catClicks);
 });
