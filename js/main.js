055bdd791b16350a7806e2b46dc03b9af0b21cd7
Micah Yunana Bala <micahkulien@gmail.com>
2018-10-01 20:33:38 +0100

    Increase number of clickable pictures

--- js/main.js
+++ js/main.js
@@ -1,58 +1,106 @@
 'use strict';
 
 class ClickCat {
-  constructor(targetCat, clicks) {
+  constructor(clickedElement, clicks) {
     this.numClicks = clicks;
-    this.targetClicked = targetCat;
+    this.targetClicked = clickedElement;
+    this.src = '';
+    this.alt = '';
   }
 
-  /* Variables */
+  updatePicture(src, alt) {
+    // Update number of clicks
+    numberOfClicks.textContent = this.numClicks;
 
-  // Method to update the clicks on the page
-  updateClicks() {
-    // Select the class holding the cat image
-    let myCat = document.querySelector(`.${this.targetClicked}`);
-    // console.log(myCat);
+    // Show the picture
+    let image = document.querySelector('.image');
+    image.setAttribute('src', src);
+    image.setAttribute('alt', alt);
+  }
 
-    //   Find the next sibling and target its child element with class of clicks - "p .clicks"
-    let nxtSibling = myCat.nextElementSibling.firstElementChild; //Gives you p tag
-    // console.log(nxtSibling);
-    let child = `${nxtSibling.tagName.toLowerCase()} .clicks`; //selects "p .clicks"
-    let elem = document.querySelector(child);
+  // Show Picture and update the number of clicks
+  showPicture() {
+    let myCat = (pictureName.textContent = this.targetClicked);
 
-    // Update the child element's text to the number of clicks
-    /* At this point, i expect that if the cat img tag with class first_cat is clicked
-    * then elem will be "first_cat p .clicks"
-     */
-
-    elem.textContent = this.numClicks;
+    switch (myCat) {
+      case 'Turkish Van':
+        src = 'img/TurkishVan.jpg';
+        alt = 'Picture of Turkish Van cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'Turkish Angora':
+        src = 'img/TurkishAngora.jpg';
+        alt = 'Picture of Turkish Angora cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'Ragadol':
+        src = 'img/Ragdol.jpg';
+        alt = 'Picture of a Ragdol cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'Persian Cat':
+        src = 'img/PersianCat.jpg';
+        alt = 'Picture of a Persian cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'Abyssinian Cat':
+        src = 'img/AbyssinianCat.jpg';
+        alt = 'Picture of a Abyssinian cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'Snow Shoe':
+        src = 'img/SnowShoe.jpg';
+        alt = 'Picture of a Snow Shoe cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'Russian Blue':
+        src = 'img/RussianBlue.jpg';
+        alt = 'Picture of a Russian Blue cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'Japanese Bobtail':
+        src = 'img/JapaneseBobtail.jpg';
+        alt = 'Picture of a Japanese Bobtail cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'American Shorthair':
+        src = 'img/AmericanShorthair.jpg';
+        alt = 'Picture of an American Shorthair cat';
+        this.updatePicture(src, alt);
+        break;
+      case 'American Wirehair':
+        src = 'img/AmericanWirehair.jpg';
+        alt = 'Picture of an American Wirehair cat';
+        this.updatePicture(src, alt);
+        break;
+    }
   }
 }
 
-// Select cat pictures
-let catPic = document.querySelectorAll('.card');
+// Select list of cat names
+let buttons = document.querySelectorAll('.buttons');
 
-// track number of clicks
+// Select container holding the cat name in the display area
+let pictureName = document.querySelector('.head .name');
+
+// Select container holding the number of clicks in the display area
+let numberOfClicks = document.querySelector('.card_footer .clicks');
+
+// track number of clicks and the clicked element
 let clicks = 0;
-let targetCat;
+let clickedElement, src, alt;
 
 // function to instantiate new ClickCat object
 function update(e) {
   clicks++;
+  clickedElement = e.target.textContent;
 
-  // Be sure we are clicking the actual cat image and not any where outside it
-  if (e.target.tagName === 'IMG') {
-    targetCat = e.target.className;
-    console.log(targetCat);
-
-    let myClicks = new ClickCat(targetCat, clicks);
-    let printOut = myClicks.updateClicks();
-
-    return printOut;
-  }
+  let picObject = new ClickCat(clickedElement, clicks);
+  picObject.showPicture();
 }
 
-catPic.forEach(cat => {
-  // console.log(cat);
-  cat.addEventListener('click', update);
+// Loop through buttons holding list of cat names
+// And listen for click event on each
+buttons.forEach(button => {
+  button.addEventListener('click', update);
 });
